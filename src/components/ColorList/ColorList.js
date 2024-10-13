6f933501b9def57a7241ee67a7923029a5844fb9
Mehebul02 <mehebul2123@gmail.com>
2024-10-13 22:45:10 +0600

    add the color list

---
+++ src/components/ColorList/ColorList.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
+import ColorItem from '../ColorItem/ColorItem';
+import './ColorList.css';
+
+// Sortable Color Item
+const SortableColorItem = SortableElement(({ color, index, onMenuOpen }) => (
+  <ColorItem color={color} index={index} onMenuOpen={onMenuOpen} />
+));
+
+// Sortable List Container
+const SortableColorList = SortableContainer(({ items, onMenuOpen }) => {
+  return (
+    <div className="kzui-color-list">
+      {items.map((color, index) => (
+        <SortableColorItem 
+          key={`color-${color.id}`} 
+          index={index} 
+          color={color} 
+          onMenuOpen={onMenuOpen}
+        />
+      ))}
+    </div>
+  );
+});
+
+const ColorList = () => {
+  // Initial color list
+  const [colors, setColors] = useState([
+    { id: 1, title: 'Red', color: '#FF0000' },
+    { id: 2, title: 'Green', color: '#00FF00' },
+    { id: 3, title: 'Blue', color: '#0000FF' },
+  ]);
+
+  // Handle sorting the colors array
+  const onSortEnd = ({ oldIndex, newIndex }) => {
+    setColors(arrayMove(colors, oldIndex, newIndex));
+  };
+
+  // Handle opening menu for individual items
+  const handleMenuOpen = (index) => {
+    console.log('Menu opened for:', index);
+  };
+
+  return (
+    <div className="kzui-color-list-container">
+      <SortableColorList items={colors} onSortEnd={onSortEnd} onMenuOpen={handleMenuOpen} />
+    </div>
+  );
+};
+
+export default ColorList;
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,12 +1,12 @@
 
-import './Tabs.scss';
+import './Tabs.css';
 
 const Tabs = () => {
   return (
     <div className="kzui-tabs">
       <div className="kzui-tab kzui-tab--active">Colors</div>
-      <div className="kzui-tab kzui-tab--disabled">Group</div>
-      <div className="kzui-tab kzui-tab--disabled">Settings</div>
+      <div className="kzui-tab kzui-tab--disabled">Group 1</div>
+      <div className="kzui-tab kzui-tab--disabled">Group 2</div>
     </div>
   );
 };
